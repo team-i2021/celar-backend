0fa2baa390da713a5542775584545dfde253373e
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2022-09-22 00:05:13 +0900

    add: maintenance mode

---
+++ src/maintenance.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import WebSocket, { WebSocketServer } from 'ws';
+import { config } from 'dotenv';
+
+config();
+
+console.log('Celar Backend Service - Maintenance Mode')
+
+const WebSocketPort = Number(process.env.WSPORT);
+const HTTPPort = Number(process.env.HTTPPORT);
+
+const app = express();
+const wss = new WebSocketServer({ port: WebSocketPort });
+
+
+wss.on('connection', function connection(ws: WebSocket) {
+    ws.send(JSON.stringify({action: "ERR", content: "UnderTheMaintenance"}));
+});
+
+app.get('/', function (req, res) {
+    res.header("Content-Type", "application/json;charset=utf-8");
+    res.status(200).send(JSON.stringify({ title: "Celar Backend Service", repository: "celar-team/celar-backend", status: "Maintenance" }));
+});
+
+console.log('Server starting...');
+console.log('HTTP: %d\nWebSocket: %d', HTTPPort, WebSocketPort);
+app.listen(HTTPPort);
